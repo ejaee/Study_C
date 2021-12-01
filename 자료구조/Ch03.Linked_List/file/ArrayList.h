823425d20f51602f763aeb9efd222cdf6be671b0
EJAE <87407504+Ejaeda@users.noreply.github.com>
2021-12-01 16:58:45 +0900

    Update ArrayList.h

--- 자료구조/Ch03.Linked_List/file/ArrayList.h
+++ 자료구조/Ch03.Linked_List/file/ArrayList.h
@@ -6,26 +6,26 @@
 
 /*** ArrayList의 정의 ****/
 #define LIST_LEN	100
-typedef int LData;
+typedef int LData;	 		    // 리스트에 int형 데이터의 저장을 위한 선언
 
 typedef struct __ArrayList
 {
-	LData arr[LIST_LEN];
-	int numOfData;
-	int curPosition;
+	LData arr[LIST_LEN];                // 데이터의 저장공간이 배열로 선언
+	int numOfData;                      // 저장된 데이터의 수 기록
+	int curPosition;                    // 데이터 참조위치 기록
 } ArrayList;
 
+/*** ArrayList와 관련된 연산들 ***/
+typedef ArrayList List;       	  	    // List는 배열 기반 리스트임을 선언
+					    // LinkedList List로 변경하면 리스트 종류가 바
 
-/*** ArrayList와 관련된 연산들 ****/
-typedef ArrayList List;
+void ListInit(List * plist);           	    // 초기화
+void LInsert(List * plist, LData data);     // 데이터를 저장
 
-void ListInit(List * plist);
-void LInsert(List * plist, LData data);
+int LFirst(List * plist, LData * pdata);    // 첫 데이터 참조
+int LNext(List * plist, LData * pdata);     // 두번째 이후 데이터 참조
 
-int LFirst(List * plist, LData * pdata);
-int LNext(List * plist, LData * pdata);
-
-LData LRemove(List * plist);
-int LCount(List * plist);
+LData LRemove(List * plist);                // 참조한 데이터 삭제
+int LCount(List * plist);                   // 저장된 데이터 수 반환
 
 #endif
