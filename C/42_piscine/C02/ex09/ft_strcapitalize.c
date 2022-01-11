86bf90d823211dd0278565d5e313ad176b63697f
최이재 <ejae8319@gmail.com>
2022-01-11 22:06:10 +0900

    Slove C02_9

--- C/42_piscine/C02/ex09/ft_strcapitalize.c
+++ C/42_piscine/C02/ex09/ft_strcapitalize.c
@@ -19,8 +19,21 @@ char	*ft_strcapitalize(char	*str)
 	idx = 0;
 	while (str[idx])
 	{
-		if (str[idx] >= 97 && str[idx] <= 122)
+		if ((str[idx] >= 97 && str[idx] <= 122) || (str[idx] >= 65 && str[idx] <= 90) 
+			|| (str[idx] >= '0' && str[idx] <= '9'))
 		{
-			ft_upper(str[idx]);
+			if (str[idx] >= 97 && str[idx] <= 122)
+				str[idx] = ft_upper(str[idx]);
+			idx++;
+			while ((str[idx] >= 97 && str[idx] <= 122) || (str[idx] >= 65 && str[idx] <= 90) 
+				|| (str[idx] >= '0' && str[idx] <= '9'))
+			{
+				if (str[idx] >= 65 && str[idx] <= 90)
+					str[idx] = ft_lower(str[idx]);
+				idx++;
+			}
 		}
-
+		idx++;
+	}
+	return (str);
+}
---
+++ C/42_piscine/C02/ex10/ft_strlcpy.c
@@ -0,0 +1,4 @@
+#include <unistd.h>
+
+
+
