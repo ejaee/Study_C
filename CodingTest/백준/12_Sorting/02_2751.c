edec472c06264bb6a2f3826738ac16cf43ecfcf5
최이재 <ejae8319@gmail.com>
2022-03-25 05:22:18 +0900

    0325_Q1

--- CodingTest/백준/12_Sorting/02_2751.c
+++ CodingTest/백준/12_Sorting/02_2751.c
@@ -1,63 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void Swap(int arr[], int idx1, int idx2)
+void MergeTwoArea(int arr[], int left, int mid, int right)
 {
-	int temp = arr[idx1];
-	arr[idx1] = arr[idx2];
-	arr[idx2] = temp;
-}	
+	int fIdx = left;
+	int rIdx = mid+1;
+	int i;
 
+	int * sortArr = (int*)malloc(sizeof(int)*(right+1));
+	int sIdx = left;
 
-int Partition(int arr[], int left, int right)
-{
-	int pivot = arr[left];    // 피벗의 위치는 가장 왼쪽! 
-	int low = left+1;
-	int high = right;
+	while(fIdx<=mid && rIdx<=right)
+	{
+		if(arr[fIdx] <= arr[rIdx])
+			sortArr[sIdx] = arr[fIdx++];
+		else
+			sortArr[sIdx] = arr[rIdx++];
 
-	while(low <= high)    // 교차되지 않을 때까지 반복
-	{	
-		while(pivot >= arr[low] && low <= right)
-			low++;
-
-		while(pivot <= arr[high] && high >= (left+1))
-			high--;
-
-		if(low <= high)    // 교차되지 않은 상태라면 Swap 실행
-			Swap(arr, low, high);    // low와 high가 가리키는 대상 교환
+		sIdx++;
 	}
 
-	Swap(arr, left, high);    // 피벗과 high가 가리키는 대상 교환
-	return high;    // 옮겨진 피벗의 위치 정보 반환
+	if(fIdx > mid)
+	{
+		for(i=rIdx; i<=right; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	else
+	{
+		for(i=fIdx; i<=mid; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+
+	for(i=left; i<=right; i++)
+		arr[i] = sortArr[i];
+	
+	free(sortArr);
 }
 
-void QuickSort(int arr[], int left, int right)
+void MergeSort(int arr[], int left, int right)
 {
-	if(left <= right)
+	int mid;
+
+	if(left < right)
 	{
-		int pivot = Partition(arr, left, right);    // 둘로 나눠서 
-		QuickSort(arr, left, pivot-1);    // 왼쪽 영역을 정렬
-		QuickSort(arr, pivot+1, right);    // 오른쪽 영역을 정렬
+		// 중간 지점을 계산한다.
+		mid = (left+right) / 2;
+
+		// 둘로 나눠서 각각을 정렬한다.
+		MergeSort(arr, left, mid);
+		MergeSort(arr, mid+1, right);
+
+		// 정렬된 두 배열을 병합한다.
+		MergeTwoArea(arr, left, mid, right);
 	}
 }
 
 int main(void)
 {
-	int N, idx;
-	int arr[1000000] = {0};
+    int N, idx;
+    int arr[1000000] = {0};
 
-	scanf("%d", &N);
+    scanf("%d", &N);
 
-	idx = -1;
-	while (++idx < N)
-		scanf("%d", &arr[idx]);
+    idx = -1;
+    while (++idx < N)
+        scanf("%d", &arr[idx]);
 
-	int len = idx;
-	int i;
+    int len = idx;
+    int i;
 
-	QuickSort(arr, 0, len-1);
+    MergeSort(arr, 0, len-1);
 
-	for(i=0; i<len; i++)
-		printf("%d\n", arr[i]);
+    for(i=0; i<len; i++)
+        printf("%d\n", arr[i]);
 
-	return 0;
+    return 0;
 }
