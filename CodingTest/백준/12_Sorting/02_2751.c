05fb5273cd7b171fc583deb7f374b1ecebe3f536
최이재 <ejae8319@gmail.com>
2022-03-25 05:11:13 +0900

    0325_Q1

--- CodingTest/백준/12_Sorting/02_2751.c
+++ CodingTest/백준/12_Sorting/02_2751.c
@@ -1,37 +1,63 @@
 #include <stdio.h>
-void	Quick(int arr[], int left, int right){
-	int L = left, R =right;
-	int temp;
-	int pivot = arr[(left + right) /2];
-	while(L<=R){
-		while(arr[L] < pivot)
-			L++;
-		while(arr[R] > pivot)
-			R--;
-		if(L <= R){
-			if(L != R){
-				temp = arr[L];
-				arr[L] = arr[R];
-				arr[R] = temp;
-			}
-			L++;
-			R++;
-			for(int idx = 0; idx < right; idx++){
-				printf("%d\n", arr[idx]);
-			}
-		}
+
+void Swap(int arr[], int idx1, int idx2)
+{
+	int temp = arr[idx1];
+	arr[idx1] = arr[idx2];
+	arr[idx2] = temp;
+}	
+
+
+int Partition(int arr[], int left, int right)
+{
+	int pivot = arr[left];    // 피벗의 위치는 가장 왼쪽! 
+	int low = left+1;
+	int high = right;
+
+	while(low <= high)    // 교차되지 않을 때까지 반복
+	{	
+		while(pivot >= arr[low] && low <= right)
+			low++;
+
+		while(pivot <= arr[high] && high >= (left+1))
+			high--;
+
+		if(low <= high)    // 교차되지 않은 상태라면 Swap 실행
+			Swap(arr, low, high);    // low와 high가 가리키는 대상 교환
+	}
+
+	Swap(arr, left, high);    // 피벗과 high가 가리키는 대상 교환
+	return high;    // 옮겨진 피벗의 위치 정보 반환
+}
+
+void QuickSort(int arr[], int left, int right)
+{
+	if(left <= right)
+	{
+		int pivot = Partition(arr, left, right);    // 둘로 나눠서 
+		QuickSort(arr, left, pivot-1);    // 왼쪽 영역을 정렬
+		QuickSort(arr, pivot+1, right);    // 오른쪽 영역을 정렬
 	}
 }
 
+int main(void)
+{
+	int N, idx;
+	int arr[1000000] = {0};
 
-int	main(){
-	int N, arr[1000000];
 	scanf("%d", &N);
-	while(N--){
-		scanf("%d", &N);
-	}
-	arr[N] = 0;
-	Quick(arr, 0, N-1);
+
+	idx = -1;
+	while (++idx < N)
+		scanf("%d", &arr[idx]);
+
+	int len = idx;
+	int i;
+
+	QuickSort(arr, 0, len-1);
+
+	for(i=0; i<len; i++)
+		printf("%d\n", arr[i]);
 
 	return 0;
 }
