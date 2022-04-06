a070d7cd6b46f9aa867da7a26517d16dce49aee0
최이재 <ejae8319@gmail.com>
2022-04-06 22:34:39 +0900

    Q1

--- C-CodingTest/백준/07_String/1212.c
+++ C-CodingTest/백준/07_String/1212.c
@@ -1,59 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main()
+int	main()
 {
-	int num;
-	int idx;
-	int len;
-	char *base = "01";
-	char *base10 = "0123456789";
-	char stack1[1000000] = {0};
-	char stack2[1000000] = {0};
-	char *res2;
+	char	str[333335] = {0};
+	char	first[9][4] = {"", "1", "10", "11", "100", "101", "110", "111"};
+	char	two[9][4] = {"000", "001", "010", "011", "100", "101", "110", "111"};
+	int	len;
+	int	idx;
 
-	num = 314;
-//	scanf("%d", &num);
+	scanf("%s", str);
+	len = strlen(str);
 
-	
+	printf("%s", first[str[0] - '0']);
+
 	idx = 0;
-	len = 0;
-	while (num >= 0)
-	{
-		stack1[idx++] = num % 10;
-		num /= 10;
-		len++;
-		if (num == 0)
-			break;
-	}
-
-
-	num = 0;
-	idx = -1;
 	while (++idx < len)
-		num = num * 10 + base10[stack1[idx]];
-printf("num10 : %d\n", num);
-	idx = 0;
-	len = 0;
-	while (num >= 0)
-	{
-		stack2[idx++] = num % 2;
-printf("stack[] : %d\n", num % 2);
-		num /= 2;
-		len++;
-		if (num == 0)
-			break;
-	}
-
-	res2 = malloc(sizeof(char) * (len + 1));
-	idx = -1;
-	while (++idx < len)
-		res2[idx] = stack2[idx];
-	res2[idx] = 0;
-	
-	idx = -1;
-	while (++idx < len)
-		printf("%d", res2[idx]);
-
+		printf("%s", two[str[idx] - '0']);
 	return (0);
 }
